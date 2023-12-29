4218ea80c6bf0257b16a921deae3a65bd451d63f
ansumu <ansumu@yahoo.es>
2023-12-29 19:48:04 +0100

    Merge branch 'develop'

---
+++ app/src/main/java/com/ansumu/pruebatecnica/Ficha/components/DetFicha.kt
@@ -0,0 +1,73 @@
+package com.ansumu.pruebatecnica.Ficha.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ansumu.pruebatecnica.R
+
+@Composable
+fun DetFicha(imagePainter: Painter, titulo: String, dato: String) {
+    Row(modifier=Modifier
+        .fillMaxWidth()
+        .height(65.dp)) {
+        Image(
+            painter = imagePainter,
+            contentDescription = "image description",
+            modifier = Modifier
+                .padding(1.dp)
+                .width(32.dp)
+                .height(32.dp),
+            contentScale = ContentScale.Fit
+        )
+        Column(modifier=Modifier.padding(end=20.dp)) {
+            Text(
+                text = titulo,
+                style = TextStyle(
+                    fontSize = 11.sp,
+                    lineHeight = 14.sp,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF707070),
+                )
+            )
+            Spacer(modifier=Modifier.height(5.dp))
+            Text(
+                text = dato,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 16.sp,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFF000000),
+                )
+            )
+            Spacer(modifier=Modifier.height(10.dp))
+            Image(
+                painter = painterResource(id = R.drawable.linea),
+                contentDescription = "image description",
+                modifier = Modifier
+                    .border(width = 1.dp, color = Color(0xFFE7E7E7), shape = RoundedCornerShape(size = 3.dp))
+                    .fillMaxWidth(1f)
+                    .height(1.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+
